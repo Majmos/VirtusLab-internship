c003a70cfce84147881ae82e0d7ba59d866d7382
Majmos <mimsowy@gmail.com>
2022-04-06 22:59:39 +0200

    The ReceiptGenerator now works correctly

--- src/main/java/com/virtuslab/internship/receipt/ReceiptGenerator.java
+++ src/main/java/com/virtuslab/internship/receipt/ReceiptGenerator.java
@@ -1,14 +1,16 @@
 package com.virtuslab.internship.receipt;
 
 import com.virtuslab.internship.basket.Basket;
-
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ReceiptGenerator {
 
     public Receipt generate(Basket basket) {
-        List<ReceiptEntry> receiptEntries = new ArrayList<>();
+        var products = basket.getProducts();
+        var productsMap = products.stream().collect(Collectors.toMap(product -> product, product -> 1, Integer::sum));
+        List<ReceiptEntry> receiptEntries = productsMap.entrySet().stream().map(entry -> new ReceiptEntry(entry.getKey(), entry.getValue())).collect(Collectors.toList());
+
         return new Receipt(receiptEntries);
     }
 }
